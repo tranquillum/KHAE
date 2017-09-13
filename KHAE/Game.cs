aeeca0e17d0fa28f0eafc616e6684b82ce1e0266
tranquillum <31825404+tranquillum@users.noreply.github.com>
2017-09-13 20:16:31 +0300

    ?

--- KHAE/Game.cs
+++ KHAE/Game.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,8 +23,7 @@ namespace KHAE
         double samm = 0;
         string lang = "";
         double kord = 0;
-        string texttick = "";
-        int i = 0;
+
 
         public Game()
         {
@@ -44,56 +42,36 @@ namespace KHAE
 
             alusta.Text = settings.startbtn(lang);
             this.Text = "Game " + lang;
-
-            
         }
 
         
         
         private void button1_Click(object sender, EventArgs e)//vasakpoolne nupp x.1
         {
-
             if (samm == 1)
-            {
-                texttik(startLoc.startLoc_Samm_1(lang, 1.1));//(lang, 1.1) lang - keel, 1.1 - vastuse variant(1- samm, 1-varian)
+            {         
+                Story.Text += startLoc.startLoc_Samm_1(lang, 1.1) + Environment.NewLine;//(lang, 1.1) lang - keel, 1.1 - vastuse variant(1- samm, 1-varian)
                 samm = 2;
                 kord = 0;
                 v1.Text = startLoc.bteV1(lang);
                 v2.Text = startLoc.bteV2(lang);
                 mianpic.Image = kapppic.Image;
             }
-
             if (samm==2)
             {
 
 
             }
-
-
-
-
-
-
-
-
-
-
-            if (Story.Visible)
-            {
                 
-                Story.SelectionStart = Story.Text.Length;
-                Story.ScrollToCaret();
-            }
         }
         
         private void button2_Click(object sender, EventArgs e)//parempoolne nupp x.2
         {
             if (samm == 1)
             {
+                
+                Story.Text += startLoc.startLoc_Samm_1(lang, 1.2) + Environment.NewLine;
                 kord++;
-
-                
-                
                 if (kord ==4)
                 {
                     Story.Text = "";
@@ -101,24 +79,7 @@ namespace KHAE
                     Story.Text += Environment.NewLine+ startLoc.startLoc_Surm(lang) + Environment.NewLine;
                     v1.Visible = false;
                     v2.Visible = false;
-                    kord = 0;
                 }
-                else
-                {
-                    texttik(startLoc.startLoc_Samm_1(lang, 1.2));
-                }
-            }
-
-
-
-
-
-
-            if (Story.Visible)//hold scroll down
-            {
-                
-                Story.SelectionStart = Story.Text.Length;
-                Story.ScrollToCaret();
             }
 
         }
@@ -127,8 +88,8 @@ namespace KHAE
         {
             if (alusta.Text == settings.startbtn(lang))
             {
-                texttik(startText.startText_Samm_0(lang));
- 
+                Story.Text += startText.startText_Samm_0(lang) + Environment.NewLine;
+                
                 samm = 1;
                 alusta.Text = settings.resbtn(lang);
                 v1.Text = startText.bteV1(lang);
@@ -153,43 +114,5 @@ namespace KHAE
         {
 
         }
-        
-
-
-
-
-
-
-
-        //teksti taimer
-        public void texttik(string text)
-        {
-            timer1.Enabled = true;
-            texttick = text;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {         
-            if(i< texttick.Length)
-            {             
-                Story.Text += texttick[i];
-                i++;
-                v1.Enabled = false;
-                v2.Enabled = false;
-                alusta.Enabled = false;
-            }
-            else
-            {
-                i = 0;
-                timer1.Enabled = false;
-                v1.Enabled = true;
-                v2.Enabled = true;
-                alusta.Enabled = true;
-            }         
-        }
-
-
-
-
     }
 }
