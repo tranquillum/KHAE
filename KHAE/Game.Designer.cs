46c2161316d213eef294fdfc0d0fdf055572db8d
tranquillum <31825404+tranquillum@users.noreply.github.com>
2017-09-13 20:36:10 +0300

    try to fix

--- KHAE/Game.Designer.cs
+++ KHAE/Game.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.v1 = new System.Windows.Forms.Button();
             this.v2 = new System.Windows.Forms.Button();
             this.alusta = new System.Windows.Forms.Button();
@@ -35,6 +36,7 @@
             this.kapppic = new System.Windows.Forms.PictureBox();
             this.mainroompic = new System.Windows.Forms.PictureBox();
             this.mianpic = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.kapppic)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainroompic)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mianpic)).BeginInit();
@@ -42,10 +44,9 @@
             // 
             // v1
             // 
-            this.v1.Location = new System.Drawing.Point(531, 705);
-            this.v1.Margin = new System.Windows.Forms.Padding(4);
+            this.v1.Location = new System.Drawing.Point(398, 573);
             this.v1.Name = "v1";
-            this.v1.Size = new System.Drawing.Size(279, 49);
+            this.v1.Size = new System.Drawing.Size(209, 40);
             this.v1.TabIndex = 1;
             this.v1.Text = "*****";
             this.v1.UseVisualStyleBackColor = true;
@@ -53,10 +54,9 @@
             // 
             // v2
             // 
-            this.v2.Location = new System.Drawing.Point(817, 705);
-            this.v2.Margin = new System.Windows.Forms.Padding(4);
+            this.v2.Location = new System.Drawing.Point(613, 573);
             this.v2.Name = "v2";
-            this.v2.Size = new System.Drawing.Size(288, 49);
+            this.v2.Size = new System.Drawing.Size(216, 40);
             this.v2.TabIndex = 2;
             this.v2.Text = "*****";
             this.v2.UseVisualStyleBackColor = true;
@@ -64,10 +64,9 @@
             // 
             // alusta
             // 
-            this.alusta.Location = new System.Drawing.Point(32, 705);
-            this.alusta.Margin = new System.Windows.Forms.Padding(4);
+            this.alusta.Location = new System.Drawing.Point(24, 573);
             this.alusta.Name = "alusta";
-            this.alusta.Size = new System.Drawing.Size(275, 44);
+            this.alusta.Size = new System.Drawing.Size(206, 36);
             this.alusta.TabIndex = 4;
             this.alusta.Text = "alusta";
             this.alusta.UseVisualStyleBackColor = true;
@@ -76,24 +75,25 @@
             // Story
             // 
             this.Story.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.Story.Location = new System.Drawing.Point(531, 15);
-            this.Story.Margin = new System.Windows.Forms.Padding(4);
+            this.Story.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Story.Location = new System.Drawing.Point(398, 12);
             this.Story.MaxLength = 999999999;
             this.Story.Multiline = true;
             this.Story.Name = "Story";
             this.Story.ReadOnly = true;
             this.Story.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.Story.Size = new System.Drawing.Size(573, 661);
+            this.Story.Size = new System.Drawing.Size(431, 538);
             this.Story.TabIndex = 5;
+            this.Story.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Story.UseWaitCursor = true;
             // 
             // kapppic
             // 
             this.kapppic.BackgroundImage = global::KHAE.Properties.Resources.kapp;
             this.kapppic.Image = global::KHAE.Properties.Resources.kapp;
-            this.kapppic.Location = new System.Drawing.Point(1189, 209);
-            this.kapppic.Margin = new System.Windows.Forms.Padding(4);
+            this.kapppic.Location = new System.Drawing.Point(1103, 110);
             this.kapppic.Name = "kapppic";
-            this.kapppic.Size = new System.Drawing.Size(132, 113);
+            this.kapppic.Size = new System.Drawing.Size(99, 92);
             this.kapppic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.kapppic.TabIndex = 7;
             this.kapppic.TabStop = false;
@@ -102,10 +102,9 @@
             // mainroompic
             // 
             this.mainroompic.Image = global::KHAE.Properties.Resources.main;
-            this.mainroompic.Location = new System.Drawing.Point(1189, 65);
-            this.mainroompic.Margin = new System.Windows.Forms.Padding(4);
+            this.mainroompic.Location = new System.Drawing.Point(1103, 12);
             this.mainroompic.Name = "mainroompic";
-            this.mainroompic.Size = new System.Drawing.Size(132, 113);
+            this.mainroompic.Size = new System.Drawing.Size(99, 92);
             this.mainroompic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.mainroompic.TabIndex = 6;
             this.mainroompic.TabStop = false;
@@ -114,20 +113,24 @@
             // mianpic
             // 
             this.mianpic.Image = global::KHAE.Properties.Resources.main;
-            this.mianpic.Location = new System.Drawing.Point(16, 15);
-            this.mianpic.Margin = new System.Windows.Forms.Padding(4);
+            this.mianpic.Location = new System.Drawing.Point(12, 12);
             this.mianpic.Name = "mianpic";
-            this.mianpic.Size = new System.Drawing.Size(473, 662);
+            this.mianpic.Size = new System.Drawing.Size(355, 538);
             this.mianpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.mianpic.TabIndex = 3;
             this.mianpic.TabStop = false;
             this.mianpic.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1117, 786);
+            this.ClientSize = new System.Drawing.Size(1214, 639);
             this.Controls.Add(this.kapppic);
             this.Controls.Add(this.mainroompic);
             this.Controls.Add(this.Story);
@@ -135,7 +138,6 @@
             this.Controls.Add(this.mianpic);
             this.Controls.Add(this.v2);
             this.Controls.Add(this.v1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Game";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -155,6 +157,7 @@
         private System.Windows.Forms.TextBox Story;
         private System.Windows.Forms.PictureBox mainroompic;
         private System.Windows.Forms.PictureBox kapppic;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- KHAE/Game.cs
+++ KHAE/Game.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -23,7 +24,8 @@ namespace KHAE
         double samm = 0;
         string lang = "";
         double kord = 0;
-
+        string texttick = "";
+        int i = 0;
 
         public Game()
         {
@@ -42,36 +44,56 @@ namespace KHAE
 
             alusta.Text = settings.startbtn(lang);
             this.Text = "Game " + lang;
+
+            
         }
 
         
         
         private void button1_Click(object sender, EventArgs e)//vasakpoolne nupp x.1
         {
+
             if (samm == 1)
-            {         
-                Story.Text += startLoc.startLoc_Samm_1(lang, 1.1) + Environment.NewLine;//(lang, 1.1) lang - keel, 1.1 - vastuse variant(1- samm, 1-varian)
+            {
+                texttik(startLoc.startLoc_Samm_1(lang, 1.1));//(lang, 1.1) lang - keel, 1.1 - vastuse variant(1- samm, 1-varian)
                 samm = 2;
                 kord = 0;
                 v1.Text = startLoc.bteV1(lang);
                 v2.Text = startLoc.bteV2(lang);
                 mianpic.Image = kapppic.Image;
             }
+
             if (samm==2)
             {
 
 
             }
+
+
+
+
+
+
+
+
+
+
+            if (Story.Visible)
+            {
                 
+                Story.SelectionStart = Story.Text.Length;
+                Story.ScrollToCaret();
+            }
         }
         
         private void button2_Click(object sender, EventArgs e)//parempoolne nupp x.2
         {
             if (samm == 1)
             {
-                
-                Story.Text += startLoc.startLoc_Samm_1(lang, 1.2) + Environment.NewLine;
                 kord++;
+
+                
+                
                 if (kord ==4)
                 {
                     Story.Text = "";
@@ -79,7 +101,24 @@ namespace KHAE
                     Story.Text += Environment.NewLine+ startLoc.startLoc_Surm(lang) + Environment.NewLine;
                     v1.Visible = false;
                     v2.Visible = false;
+                    kord = 0;
                 }
+                else
+                {
+                    texttik(startLoc.startLoc_Samm_1(lang, 1.2));
+                }
+            }
+
+
+
+
+
+
+            if (Story.Visible)//hold scroll down
+            {
+                
+                Story.SelectionStart = Story.Text.Length;
+                Story.ScrollToCaret();
             }
 
         }
@@ -88,8 +127,8 @@ namespace KHAE
         {
             if (alusta.Text == settings.startbtn(lang))
             {
-                Story.Text += startText.startText_Samm_0(lang) + Environment.NewLine;
-                
+                texttik(startText.startText_Samm_0(lang));
+ 
                 samm = 1;
                 alusta.Text = settings.resbtn(lang);
                 v1.Text = startText.bteV1(lang);
@@ -114,5 +153,43 @@ namespace KHAE
         {
 
         }
+        
+
+
+
+
+
+
+
+        //teksti taimer
+        public void texttik(string text)
+        {
+            timer1.Enabled = true;
+            texttick = text;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {         
+            if(i< texttick.Length)
+            {             
+                Story.Text += texttick[i];
+                i++;
+                v1.Enabled = false;
+                v2.Enabled = false;
+                alusta.Enabled = false;
+            }
+            else
+            {
+                i = 0;
+                timer1.Enabled = false;
+                v1.Enabled = true;
+                v2.Enabled = true;
+                alusta.Enabled = true;
+            }         
+        }
+
+
+
+
     }
 }
