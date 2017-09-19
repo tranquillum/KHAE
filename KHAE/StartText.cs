a1857011783f30532b3b0829d9f571877a0ef42b
tranquillum <31825404+tranquillum@users.noreply.github.com>
2017-09-19 15:43:02 +0300

    UTF8 works good

--- KHAE/StartText.cs
+++ KHAE/StartText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace KHAE
 {
@@ -10,15 +11,7 @@ namespace KHAE
     {
         //Samm 0
         //EST
-        string sammSamm0V1EST = "#Chapter I : Start of K.H.A.E" + Environment.NewLine+Environment.NewLine+ 
-            "#!?%!#!&:'Aeg, see on imelik asi, kas ole? Sa ei tunne seda, Sa ei saa seda haarata.." +
-            "Kui sul poleks kella ei teaks, et see eksisteeriks, kuid siiski ta juhib meid. " +
-            "See muudab meie käitumist tavapärasest, ta paneb meid tegema asju mida me tavaliselt ei teeks. " +
-            "Mõned inimesed nad..nad ei saa aru selle võimust, mis tal on meie üle. " +
-            "Me oleme kui nukk tema kätes, pole vahet kes sa oled või kus sa oled..Äratus'"+ Environment.NewLine + Environment.NewLine + 
-            "*Kõik on pime*" + Environment.NewLine+ 
-            "*Sa kuuled valju sahinat*" + Environment.NewLine + 
-            "*Sa tunned külma põrandat enda all*";
+        string sammSamm0V1EST = File.ReadAllText(@"..\..\GameTexts\StartLoc.txt", System.Text.Encoding.UTF8);
 
         string btnSamm0V1EST = "Ava Silmad";
         string btnSamm0V2EST = "Lama Edasi";
@@ -37,7 +30,7 @@ namespace KHAE
         }
 
 
-        public string btnSamm0(string lang, int versioon) //nuppu 1 uus tekst
+        public string btnSamm0(string lang, int versioon) 
         {
 
             if ( versioon ==1)
